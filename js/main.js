840b1b7d40f8b6fc0ebf1adc10b4802568a911b3
Rodrigo <rodrigo,marzullo@gmail.com>
2018-01-10 00:59:34 -0200

    concertando alguns bugs

--- js/main.js
+++ js/main.js
@@ -9,13 +9,12 @@ $("#submit-button").on("click", function(event){
 
   event.preventDefault();
 
-
   var search = "media_type=image";
 
   var input = $("#input-text").val();
 
   var key = "uP4YNNyNHkpy9WscSm4Sh2HkaGP3mUxnekKC1vcn" ;
-  //+"&"+"api_key="+ key
+    //+"&"+"api_key="+ key
   var xhr = new XMLHttpRequest() ;
 
     let url = "https://images-api.nasa.gov/search?q=";
@@ -32,51 +31,54 @@ $("#submit-button").on("click", function(event){
     var resposta = JSON.parse(xhr.responseText).collection.items.map ( n =>
       n.href ) ;
 
+    var ar = [] ;
+
     for (var i = 0; i < resposta.length; i++) {
       var divRender = document.getElementById("image-reder");
       var request = new XMLHttpRequest() ;
-      request.open("GET",resposta[i], true ) ;
-      request.send() ;
+      if(resposta[i] != null ){
 
-      request.addEventListener("load", function(n){
+        request.open("GET",resposta[i], true ) ;
+        request.send() ;
 
-
-      let regex = /http:[^\s]+medium\.jpg/g ;
-      var result = regex.exec(n.target.responseText);
-      console.log(result[0]);
-
-      if(result[0] != null ){
-      let render = new imageRender (divRender) ;
-      render.update (result[0]);
-}
-
-      }, false) ;
+        request.addEventListener("load", function(n){
 
 
+          let regex = /http:[^\s]+medium\.jpg/g ;
+          var result = regex.exec(n.target.responseText);
 
+          if (result[0] != null ) {
+            ar.push(result[0]);
+          }
+
+
+        }, false) ;
+
+      // console.log(result[0]);
+
+      // if(result[i] != null ){
+      // let render = new imageRender (divRender) ;
+      // render.update (result[0]);
+    // }
 
     }
+  }
+    console.log(ar);  }
 
 
 
     var divRender = document.querySelector('#image-render') ;
-
     // var resp2 = resposta.collection.items
     //
     // var resp3 = resp2.map(function(n){
     //   return n.href ;
 
-
-
     // let teste = new imageRender (divRender)  ;
     //
     // teste.update("Menino");
+    xhr.addEventListener("load", lidaResposta, false) ;
 
 
 
-
-  }
-
-  xhr.addEventListener("load", lidaResposta, false) ;
 
 });
